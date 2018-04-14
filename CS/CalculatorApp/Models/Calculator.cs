57fae6076dcd6a4f6e5bfbb2f5b5db39c72961f0
LuGeorgiev <lubomir_78@abv.bg>
2018-04-14 22:28:15 +0300

    CSharp Calculator and book library

---
+++ CS/CalculatorApp/Models/Calculator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalculatorApp.Models
+{
+    public class Calculator
+    {
+        public decimal LeftOperand { get; set; }
+        public decimal RightOperand { get; set; }
+        public string Operator { get; set; }
+        public decimal Result { get; set; }
+
+        public Calculator()
+        {
+            this.Result = 0;
+        }
+
+        internal void CalculateResult()
+        {
+            if (this.Operator=="+")
+            {
+                this.Result = this.LeftOperand + this.RightOperand;
+            }
+            else if (this.Operator == "-")
+            {
+                this.Result = this.LeftOperand - this.RightOperand;
+            }
+            else if (this.Operator == "*")
+            {
+                this.Result = this.LeftOperand * this.RightOperand;
+            }
+            else if (this.Operator == "/")
+            {
+                this.Result = this.LeftOperand / this.RightOperand;
+            }
+        }
+    }
+}
